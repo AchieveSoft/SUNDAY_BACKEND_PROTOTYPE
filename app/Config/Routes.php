5c2de0a35b3c184132e0c7f8afa5fe941271440d
FernChanya12 <58017572+FernChanya12@users.noreply.github.com>
2025-03-06 18:29:11 +0700

    [Fern] Update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('pipeline/get-pipelines', 'PipelineController::getPipelines');
+$routes->post('pipeline/add-pipeline', 'PipelineController::addPipeline');
+$routes->get('pipeline/get-pipelines-history', 'PipelineController::getPipelinesHistory');
+$routes->post('pipeline/add-pipeline-history', 'PipelineController::addPipelineHistory');
+
